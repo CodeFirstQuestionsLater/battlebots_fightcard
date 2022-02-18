aac29a2569b833e09e41727a459573d345f4d349
Cody Ruggiero <codyruggiero@gmail.com>
2022-02-18 02:30:36 -0500

    Moved to using code by Zell. Need to add notes

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-const cheerio = require('cheerio'); //The tool for parsing data from the webpages
-const request = require('request');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+const fs = require("fs");
+const writeStream = fs.createWriteStream("scrape.csv");
 
-request('https://battlebots.fandom.com/wiki/Discovery_Season_6',
-        (error, response, html) => {
-            if(!error && response.statusCode == 200) {
-                const $ = cheerio.load(html);
-                const botTable = $('table.article-table.fandom-table > tbody > tr > td').each((i, el) => {
-                    const competitor = $(el)
-                    .text()
-                    .replace(/\s\s+/g, '') //Attmpting to remove the lines between the entries. No luck yet.
-                    //.replace(/\s/s+/g)
-                    console.log(competitor)
-                    //const competitorUrl = $(el).find('')
-                })
-                const episode = $('.mw-parser-output > h3').each((i, el) => {
-                    const episode = $(el)
-                    .text()
-                    console.log(episode)
-                })
-                //const episode2 = $('.mw-parser-output').each((i,el)=> {
-                //    const otherepisode = $(el)
-                //    .find('h3')
-                //    .text()
-                //    console.log(otherepisode)
-                //})
+const port = process.env.PORT || 4000;
 
-            }
-        })
+const app = express();
+
+const url = "https://battlebots.fandom.com/wiki/Discovery_Season_6";
+
+const peopleArr = ["joe", "bob", "sue", "cody"];
+
+axios
+  .get(url)
+  .then((res) => {
+    const $ = cheerio.load(res.data);
+    $(".article-table").each((index, element) => {
+      const botTitles = $(element).find("td").text();
+      let botArr = botTitles.split(" vs. ");
+      console.log(`${botArr}`);
+
+      writeStream.write(`${botArr} \n`);
+    });
+  })
+  .catch((err) => console.error(err));
+
+app.listen(port, () => {
+  console.log(`Server Established and running on Port ${port}`);
+});
